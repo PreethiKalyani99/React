7ddd23da7d29171d93e259bc733e8bec8f2671cd
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-03-12 15:26:26 +0530

    refactored singleDayForecast

--- src/WeatherApp/SingleDayForecast.js
+++ src/WeatherApp/SingleDayForecast.js
@@ -4,7 +4,7 @@ import { Container} from "react-bootstrap";
 
 export function SingleDayForecast(props){
     const data = props.weatherData
-
+    console.log(data.weather[0].description, 'weather')
     const getDay = (value) => {
         const date = new Date(value);
         const options = { weekday: 'long' };
@@ -17,12 +17,24 @@ export function SingleDayForecast(props){
         const fahrenheit = ((value - 273.15) * 9)/5 + 32;
         return Math.round(fahrenheit)
     }
+
+    const getWeatherIcon = (description) => {
+        const iconMap = {
+            'light rain': 'wi-sprinkle',
+            'overcast clouds': 'wi-cloudy',
+            'moderate rain': 'wi-sprinkle',
+            'scattered clouds': 'wi-cloudy',
+            'clear sky': 'wi-day-sunny'
+        }
+        return iconMap[description] || 'wi-cloud'
+    }
+    
     return (
         <>
             <Container className="card-container" key={props.i}>
                 <div className="fs-3"><b>{getDay(data.dt_txt)}</b></div>
                 <div className="fontColor">{getFormattedDate(data.dt_txt)}</div>
-                <img src ={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt="weather icon" className="weather-icon "/>
+                <img src ={require(`../utils/assets/icons/${getWeatherIcon(data.weather[0].description)}.svg`)} alt="weather icon" className="weather-icon "/>
                 <div className="fs-3"><b>{convertToFahrenheit(data.main.temp)}°F</b></div>
                 <div className="pb-2">{data.weather[0].description}</div>    
             </Container>
