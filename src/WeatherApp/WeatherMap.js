5345ca8d5f0e2145f22b10abd3248775ae40bb25
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-03-12 15:27:09 +0530

    refactored weatherMap

--- src/WeatherApp/WeatherMap.js
+++ src/WeatherApp/WeatherMap.js
@@ -1,26 +1,28 @@
 import React, {useState} from "react";
 import { SearchBar } from "./SearchBar";
-import { DisplayWeather } from "./DisplayWeather";
+import { WeatherForecast } from "./DisplayWeather";
 
 export function WeatherMap(){
     const [input, setInput] = useState("")
-    const [data, setData] = useState([])
-    const [cityInfo, setCityInfo] = useState('')
-    const baseURL = "https://api.openweathermap.org/data/2.5/forecast?q="
+    const [weatherForecast, setWeatherForecast] = useState({});
+    
+    const baseURL = "https://api.openweathermap.org/"
     const appid = "0d0fddeab44b7e1744c3e3034d1e8e96"
 
     const cityData = async function(){
         try{
-            const response = await fetch(`${baseURL}${input}&appid=${appid}`)
-            if(response){
-                const jsonData = await response.json()
-                const weather_data = jsonData?.list
-                setCityInfo(`${jsonData?.city.name}, ${jsonData?.city.country}`)
-                setData([weather_data[0], weather_data[8], weather_data[16], weather_data[24], weather_data[32]])
+            const response = await fetch(`${baseURL}data/2.5/forecast?q=${input}&appid=${appid}`)
+            if(!response.ok){
+                throw new Error("Network response is not ok")
             }
-            else{
-                throw new Error('Failed to fetch data')
+            const jsonData = await response.json()
+            const weatherData = jsonData?.list
+            const weather = [weatherData[0], weatherData[8], weatherData[16], weatherData[24], weatherData[32]];
+            const weatherForecast = {
+                cityDetails: `${jsonData?.city.name}, ${jsonData?.city.country}`,
+                weather: weather
             }
+            setWeatherForecast(weatherForecast);
         }catch(error){
             console.error("Error:", error)
         }
@@ -32,10 +34,10 @@ export function WeatherMap(){
                 setInput={setInput}
                 cityData={cityData}
             />
-            <DisplayWeather
-                weatherForecast={data}
-                cityInfo={cityInfo}
-            />
+            {weatherForecast.weather && (<WeatherForecast
+                weatherForecast={weatherForecast.weather}
+                cityInfo={weatherForecast.cityDetails}
+            />)}
         </>
     )
 }
